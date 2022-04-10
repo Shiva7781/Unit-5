e74ab43c75aed3d1773658f4a54254d150fdd18c
Shiva7781 <97456472+Shiva7781@users.noreply.github.com>
2022-04-10 12:50:34 +0530

    A1

---
+++ A1_Webpack TODO/src/index.js
@@ -0,0 +1,25 @@
+import { myNote } from "./Note";
+
+import("../index.css")
+
+// creating logo
+import Logo from "../image/logo.jpg"
+
+const myLogo = new Image()
+myLogo.src = Logo
+
+// creating title of page
+const title = document.createElement("h2")
+title.textContent = "My Notes"
+
+// creating input for user
+const inputBox = document.createElement("input")
+inputBox.type = "text"
+inputBox.placeholder = "Enter text here"
+inputBox.id = ("writeNote")
+
+// appending above dom elements to root
+document.getElementById("root").append(myLogo, title, inputBox)
+
+// adding event listener to input box
+document.getElementById("writeNote").addEventListener("keypress", myNote)
